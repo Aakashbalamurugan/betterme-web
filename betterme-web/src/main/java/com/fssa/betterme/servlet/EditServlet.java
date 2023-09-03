5a28ba4b7c851b930bc4f8e46b0eb3158aaa08ce
Aakash <116252032+Aakashbalamurugan@users.noreply.github.com>
2023-09-03 13:49:17 +0530

    module 1

---
+++ betterme-web/src/main/java/com/fssa/betterme/servlet/EditServlet.java
@@ -0,0 +1,52 @@
+package com.fssa.betterme.servlet;
+
+import java.io.IOException;
+
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.fssa.betterme.model.Event;
+import com.fssa.betterme.service.EventService;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/EditServlet")
+public class EditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+//		int id =3;
+		try {
+			Event event = EventService.getEventById(id);
+			request.setAttribute("event", event);
+
+			  RequestDispatcher dispatcher = request.getRequestDispatcher("/edit.jsp");
+			    dispatcher.forward(request, response);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.getWriter().append(e.getMessage());
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
